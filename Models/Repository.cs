d6b57abba1b371d4c1076a1818620dd392f99831
Damup1988 <damup1988@gmail.com>
2021-07-25 17:16:01 +0300

    Chapter 3 - Understanding Model Binding

---
+++ Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> _responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses = _responses;
+
+        public static void AddResponce(GuestResponse response)
+        {
+            _responses.Add(response);
+        }
+    }
+}
